bc1be207bb0200b6b94e675c4aaf65c83c3a8200
viper7lol <chithanh07092004@gmail.com>
2024-12-18 11:43:47 +0700

    dâssad

---
+++ QuanLyTrongTrot/Controller/BaseController.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTrongTrot.Controller
+{
+    internal class BaseController : System.Mvc.Controller
+    {
+        public virtual object Index() => View();
+    }
+}
