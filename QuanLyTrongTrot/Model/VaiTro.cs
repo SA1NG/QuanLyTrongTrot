e9d7bbd40e3becdfcd9030e2a2002f201724aa0e
Mailinh154 <149883288+Mailinh154@users.noreply.github.com>
2024-12-21 00:10:17 +0700

    Add files via upload

---
+++ QuanLyTrongTrot/Model/VaiTro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTrongTrot.Model
+{
+    public class VaiTro
+    {
+        public int ID { get; set; }
+        public string TenVaiTro { get; set; }
+
+    }
+}
